6b08065ae67ca5aeafc08195632e9e6254bbba5d
Pavel Beletsky <pavel.beletsky@live.com>
2019-10-22 05:44:26 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,32 @@
 # Your Code Here
+
+def map (array)
+  ar = []
+  i = 0
+  while i < array.count do
+    ar << yield(array[i])
+    i += 1 
+  end
+  ar
+end
+
+
+def reduce(array, starting_point = 0)
+  b = starting_point
+  i = 0 
+  while i < array.count do
+    a = yield(array[i], b)
+    if !a
+      b = false
+    else
+      b = a
+    end
+    i += 1 
+  end
+  b
+end
+
+
+
+
+
